00b6ba85597b71dd4aa89fa8d51595a045d99c81
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-04-10 13:18:02 +0900

    feat: Post, Posts Query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,24 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # Queryのエントリーポイント
+    # TODO: graphql/queriesからmoduleをincludeする
+    field :posts, [Types::PostType], null: false, description: "すべてのPostを取得"
+    def posts
+      Post.all
+    end
 
-    # TODO: remove me
+    field :post, Types::PostType, null: false, description: "指定したidのPostを取得" do
+      argument :id, Int, required: false
+    end
+    def post(id:)
+      Post.find(id)
+    end
+
+    # sample
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
+      description: "Docの説明欄に出る内容"
     def test_field
       "Hello World!"
     end
   end
-end
+  end
