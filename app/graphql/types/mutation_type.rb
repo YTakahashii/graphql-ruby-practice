e976ed3b84e44d7f86ecc2e7ea16419b0861ba74
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-04-10 13:40:49 +0900

    feat: UpdatePost

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :update_post, mutation: Mutations::UpdatePost
     field :create_post, mutation: Mutations::CreatePost
     # TODO: remove me
     field :test_field, String, null: false,
