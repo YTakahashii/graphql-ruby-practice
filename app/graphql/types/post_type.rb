81f4fbc5c1c6779b4b0e54894926b4d7a9245afe
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-04-10 13:59:20 +0900

    feat: Comment model

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,7 +1,10 @@
 module Types
   class PostType < Types::BaseObject
+    description 'Post'
+
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: false
+    field :comments, [Types::CommentType], null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_034021) do
+ActiveRecord::Schema.define(version: 2020_04_10_044929) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_04_10_034021) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
